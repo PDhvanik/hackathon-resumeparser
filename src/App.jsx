4980cbafd72fc4f369277b63a43802ffd9ed677c
Dhvanik Patel <pateldhvanik789@gmail.com>
2024-02-29 19:11:20 +0530

    Added react-routing and logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
 import Navbar from './component/Navbar.jsx'
 import DropArea from './component/DropArea.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './component/Dashboard/Dashboard.jsx';
+import About from './component/About/About.jsx';
 function App() {
   return (
     <>
-      <Navbar/>
-      <DropArea />
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path='/' element={<DropArea />} />
+          <Route path='/Dashboard' element={<Dashboard />} />
+          <Route path='/About' element={<About />} />
+        </Routes>
+      </Router>
     </>
   )
 }
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,17 +1,15 @@
 // import React from 'react'
 import './Navbar.css'
+import logo from '../materials/sks_logo.jpg'
 export default function Navbar() {
     return (
         <div>
             <div className="navbar">
-                <a href="">
-                    <img src="./src/materials/sks_logo.jpg" alt="LOGO here" />
-                </a>
+                    <img src={logo} alt="LOGO here" />
                 <div className="navbar-content">
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>abc</li>
-                    <li>xyz</li>
+                    <li><a href='/'>Home</a></li>
+                    <li><a href='/Dashboard'>Dashboard</a></li>
+                    <li><a href='/About'>About</a></li>
                 </div>
             </div>
             <hr />
